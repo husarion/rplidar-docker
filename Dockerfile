a9b4baee189291c61b54c1d692282d9e3956c695
rafal-gorecki <126687345+rafal-gorecki@users.noreply.github.com>
2024-01-04 14:28:13 +0100

    Add namespace (#9)
    * Add namespace
    
    * Update README.md
    
    * Jakub suggestions
    
    * readme
    
    * readme
    
    * Use robot_namespace
    
    * fix

--- Dockerfile
+++ Dockerfile
@@ -2,20 +2,19 @@
 ARG ROS_DISTRO=humble
 ARG PREFIX=
 
+# =========================== package builder ===============================
 FROM husarnet/ros:$ROS_DISTRO-ros-base AS pkg-builder
-
-SHELL ["/bin/bash", "-c"]
 
 WORKDIR /ros2_ws
 
-# install everything needed
+# Setup workspace
 RUN git clone https://github.com/Slamtec/sllidar_ros2.git /ros2_ws/src/sllidar_ros2 && \
     cp /ros2_ws/src/sllidar_ros2/launch/sllidar_a1_launch.py \
        /ros2_ws/src/sllidar_ros2/launch/sllidar_launch.py && \
     rosdep update --rosdistro $ROS_DISTRO && \
     rosdep install --from-paths src --ignore-src -y
 
-# Create health check package
+# Create healthcheck package
 RUN cd src/ && \
     source /opt/ros/$ROS_DISTRO/setup.bash && \
     ros2 pkg create healthcheck_pkg --build-type ament_cmake --dependencies rclcpp sensor_msgs && \
@@ -25,34 +24,25 @@ RUN cd src/ && \
             install(TARGETS healthcheck_node DESTINATION lib/${PROJECT_NAME})' \
             /ros2_ws/src/healthcheck_pkg/CMakeLists.txt
 
-COPY ./healthcheck.cpp /ros2_ws/src/healthcheck_pkg/src/
+COPY ./husarion_utils/healthcheck.cpp /ros2_ws/src/healthcheck_pkg/src/
 
 # Build
 RUN source /opt/ros/$ROS_DISTRO/setup.bash && \
     colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release && \
+    echo $(cat /ros2_ws/src/sllidar_ros2/package.xml | grep '<version>' | sed -r 's/.*<version>([0-9]+.[0-9]+.[0-9]+)<\/version>/\1/g') > /version.txt && \
     rm -rf build log
 
-# Second stage - Deploy the built packages
-FROM husarnet/ros:${PREFIX}${ROS_DISTRO}-ros-core
+# =========================== final stage ===============================
+FROM husarnet/ros:${PREFIX}${ROS_DISTRO}-ros-core AS final-stage
 
 ARG PREFIX
-ENV PREFIX_ENV=$PREFIX
-
-SHELL ["/bin/bash", "-c"]
 
 COPY --from=pkg-builder /ros2_ws /ros2_ws
+COPY --from=pkg-builder /version.txt  /version.txt
+COPY ./husarion_utils /husarion_utils
 
-RUN echo $(cat /ros2_ws/src/sllidar_ros2/package.xml | grep '<version>' | sed -r 's/.*<version>([0-9]+.[0-9]+.[0-9]+)<\/version>/\1/g') > /version.txt
-
-# Run healthcheck in background
-RUN entrypoint_file=$(if [ -f "/ros_entrypoint.sh" ]; then echo "/ros_entrypoint.sh"; else echo "/vulcanexus_entrypoint.sh"; fi) && \
-    sed -i '/test -f "\/ros2_ws\/install\/setup.bash" && source "\/ros2_ws\/install\/setup.bash"/a \
-            ros2 run healthcheck_pkg healthcheck_node &' \
-            $entrypoint_file
-
-COPY ./healthcheck.sh /
 HEALTHCHECK --interval=2s --timeout=1s --start-period=20s --retries=1 \
-    CMD ["/healthcheck.sh"]
+    CMD ["/husarion_utils/healthcheck.sh"]
 
 # Ensure LIDAR stops spinning on container shutdown
 STOPSIGNAL SIGINT
---
+++ husarion_utils/rplidar.launch.py
@@ -0,0 +1,93 @@
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    GroupAction,
+    OpaqueFunction,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node, PushRosNamespace, SetRemap
+from launch.substitutions import EnvironmentVariable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def launch_setup(context, *args, **kwargs):
+    launch_file = LaunchConfiguration("launch_file").perform(context)
+    serial_baudrate = LaunchConfiguration("serial_baudrate").perform(context)
+    serial_port = LaunchConfiguration("serial_port").perform(context)
+    robot_namespace = LaunchConfiguration("robot_namespace").perform(context)
+    device_namespace = LaunchConfiguration("device_namespace").perform(context)
+
+    if device_namespace:
+        frame_id = device_namespace + "_link"
+    else:
+        frame_id = "laser"
+
+    rplidar_actions = []
+
+    # Remappings
+    if robot_namespace:
+        rplidar_actions.append(SetRemap("/tf", f"/{robot_namespace}/tf"))
+        rplidar_actions.append(SetRemap("/tf_static", f"/{robot_namespace}/tf_static"))
+
+    # Device Namespace
+    rplidar_actions.append(PushRosNamespace(device_namespace))
+
+    # Rplidar
+    rplidar_path = PathJoinSubstitution([FindPackageShare("sllidar_ros2"), "launch", launch_file])
+    rplidar_actions.append(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([rplidar_path]),
+            launch_arguments={
+                "frame_id": frame_id,
+                "serial_baudrate": serial_baudrate,
+                "serial_port": serial_port,
+            }.items(),
+        )
+    )
+
+    rplidar_ns = GroupAction(actions=rplidar_actions)
+
+    # Health check
+    healthcheck_node = Node(
+        package="healthcheck_pkg",
+        executable="healthcheck_node",
+        name="healthcheck_rplidar",
+        namespace=device_namespace,
+        output="screen",
+    )
+
+    return [PushRosNamespace(robot_namespace), rplidar_ns, healthcheck_node]
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "launch_file",
+                default_value="sllidar_launch.py",
+                description="Select launch file from repo `sllidar_ros2`, based on plugged lidar model",
+            ),
+            DeclareLaunchArgument(
+                "serial_baudrate",
+                default_value="115200",
+                description="Specifying usb port baudrate to connected lidar",
+            ),
+            DeclareLaunchArgument(
+                "serial_port",
+                default_value="/dev/ttyUSB0",
+                description="Specifying usb port to connected lidar",
+            ),
+            DeclareLaunchArgument(
+                "robot_namespace",
+                default_value=EnvironmentVariable("ROBOT_NAMESPACE", default_value=""),
+                description="Namespace which will appear in front of all topics (including /tf and /tf_static).",
+            ),
+            DeclareLaunchArgument(
+                "device_namespace",
+                default_value="",
+                description="Sensor namespace that will appear before all non absolute topics and TF frames, used for distinguishing multiple cameras on the same robot.",
+            ),
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
