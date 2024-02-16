5029996c1169496612df77c8d82f3da818fb55aa
Dominik Nowak <dominik.nwk@gmail.com>
2024-02-16 12:39:36 +0100

    Ros2 healthcheck fix (#10)
    * healthcheck fix
    * QoS fix

--- husarion_utils/healthcheck.cpp
+++ husarion_utils/healthcheck.cpp
@@ -13,8 +13,13 @@ public:
       : Node("healthcheck_astra"),
         last_msg_time(std::chrono::steady_clock::now()) {
 
+    // Set custom QoS settings to match the publisher
+    auto qos = rclcpp::QoS(rclcpp::QoSInitialization(RMW_QOS_POLICY_HISTORY_KEEP_LAST, 10));
+    qos.reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE);
+    qos.durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
+
     subscription_ = create_subscription<sensor_msgs::msg::LaserScan>(
-        "scan", rclcpp::SensorDataQoS().keep_last(1),
+        "scan", qos,
         std::bind(&HealthCheckNode::msgCallback, this, std::placeholders::_1));
 
     timer_ = create_wall_timer(HEALTHCHECK_PERIOD,
@@ -37,9 +42,8 @@ private:
   }
 
   void healthyCheck() {
-    std::chrono::steady_clock::time_point current_time =
-        std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_time = current_time - last_msg_time;
+    auto current_time = std::chrono::steady_clock::now();
+    auto elapsed_time = current_time - last_msg_time;
     bool is_msg_valid = elapsed_time < MSG_VALID_TIME;
 
     if (is_msg_valid) {
@@ -52,7 +56,8 @@ private:
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<HealthCheckNode>());
+  auto node = std::make_shared<HealthCheckNode>();
+  rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
 }
